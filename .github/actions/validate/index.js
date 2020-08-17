32ebb82d16188febbc3620dc0b5105eccf36dc54
Jacob <JacobMaxJaffe@gmail.com>
2020-08-17 16:09:02 -0400

    use dir

--- .github/actions/validate/index.js
+++ .github/actions/validate/index.js
@@ -1,21 +1,31 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-var fs = require('fs');
+const fs = require('fs');
+const glob = require('glob');
 const { validateYaml } = require('./validateYaml');
 
 try {
-    // `file` input defined in action metadata file
-    const file = core.getInput('file');
-    console.log(`Validating file '${file}'`);
+    const root = core.getInput('root');
+    console.log(`Validating files in directory '${root}'`);
 
-    fs.readFile(file, 'utf8', (err, data) => {
-        if (err) throw err;
-        validateYaml(data)
-            .then(() => console.log('Validation Succeeded'))
-            .catch(error => {
-                core.setFailed(error.message);
-            })
-    });
+    // options is optional
+    glob(`${root}/*.yaml|${root}/*.yml`, {}, function (er, files) {
+        if (er) {
+            console.log('Glob error reading files!');
+            core.setFailed(er.message)
+        }
+        files.forEach(f => {
+            console.log(`Validating file: ${f}`);
+            fs.readFile(file, 'utf8', (err, data) => {
+                if (err) throw err;
+                validateYaml(data)
+                    .then(() => console.log('Validation Succeeded'))
+                    .catch(error => {
+                        core.setFailed(error.message);
+                    })
+            });
+        })
+    })
 } catch (error) {
     core.setFailed(error.message);
 }
