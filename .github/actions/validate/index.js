8bf401d36c28312d96b80c0418c94a7674a972ae
Jacob <JacobMaxJaffe@gmail.com>
2020-08-17 16:26:48 -0400

    glob sync

--- .github/actions/validate/index.js
+++ .github/actions/validate/index.js
@@ -8,24 +8,18 @@ try {
     const root = core.getInput('root');
     console.log(`Validating files in directory '${root}'`);
 
-    // options is optional
-    glob(`${root}/*.yaml|${root}/*.yml`, {}, function (er, files) {
-        if (er) {
-            console.log('Glob error reading files!');
-            core.setFailed(er.message)
-        }
-        console.log('files: ', { files })
-        files.forEach(f => {
-            console.log(`Validating file: ${f}`);
-            fs.readFile(file, 'utf8', (err, data) => {
-                if (err) throw err;
-                validateYaml(data)
-                    .then(() => console.log('Validation Succeeded'))
-                    .catch(error => {
-                        core.setFailed(error.message);
-                    })
-            });
-        })
+    const files = glob.sync(`${root}/*.yaml|${root}/*.yml`, {});
+    console.log('files: ', files)
+    files.forEach(f => {
+        console.log(`Validating file: ${f}`);
+        fs.readFile(file, 'utf8', (err, data) => {
+            if (err) throw err;
+            validateYaml(data)
+                .then(() => console.log('Validation Succeeded'))
+                .catch(error => {
+                    core.setFailed(error.message);
+                })
+        });
     })
 } catch (error) {
     core.setFailed(error.message);
